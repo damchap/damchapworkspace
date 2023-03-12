db845a99c2aeff62416db6f69e545a369da8b25e
damien chapart <damienchapart@gmail.com>
2023-03-12 11:50:42 +0100

    footer change

--- packages/ui/src/Molecules/Footer/Footer.tsx
+++ packages/ui/src/Molecules/Footer/Footer.tsx
@@ -30,6 +30,7 @@ export function Footer({className}: Props) {
                 Stack utilisé : React, next, typescript
             </p>
         </div>
+
         <div className={clsx(Styles.footerNav)}>
             <div className={clsx(Styles.FooterContent, Styles.FooterCenter)}>
             <h3>Navigation</h3>
