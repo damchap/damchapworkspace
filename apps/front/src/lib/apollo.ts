f669bcc823e53aeac0de4279bee1379094fbad50
damien chapart <damienchapart@gmail.com>
2023-05-05 10:50:56 +0200

    :bug: remove dotenv for config file

--- apps/front/src/lib/apollo.ts
+++ apps/front/src/lib/apollo.ts
@@ -1,8 +1,5 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-let dotenv = require('dotenv');
-dotenv.config()
-
 const apolloClient = new ApolloClient({
     uri: process.env.URI_FOR_API + 'api/graphql',
     cache: new InMemoryCache(),
